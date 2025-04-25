7e9d3b4b7064acf4a026ef3ef3927de15e2eb057
IONHAX04 <thirukumara04@gmail.com>
2025-04-25 20:59:54 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'reachon-express',
+  webDir: 'dist'
+};
+
+export default config;
